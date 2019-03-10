922eaf55a0b31a90fabda8ae3555707c31399c33
fuyunwang <beautifulsoup@163.com>
2019-03-10 19:13:22 +0800

    Project init

---
+++ src/main/java/com/beautifulsoup/chengfeng/ChengfengApplication.java
@@ -0,0 +1,13 @@
+package com.beautifulsoup.chengfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengfengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChengfengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beautifulsoup/chengfeng/ChengfengApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beautifulsoup.chengfeng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChengfengApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
